ae397b060e6d4757ea055c448d118060475b80a1
_TheMarCraft_ <marvin.niermann08@gmail.com>
2025-08-08 11:31:42 +0200

    updated README.md

--- src/main/java/de/themarcraft/ai/Usage.java
+++ src/main/java/de/themarcraft/ai/Usage.java
@@ -4,7 +4,7 @@ import de.themarcraft.ai.AI;
 
 public class Usage {
     public static void main(String[] args) {
-        AI test = new AI("");
+        AI test = new AI("AIzaSyAG_RILmYHA1yiD1klkQ3xrAXdmMNYOQ0c");
         System.out.println(test.newQuery("What is AI? In two short Paragraphs"));
     }
 }
